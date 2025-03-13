2c105857bcc46cebaeb1f241c7c3a2c38b68d331
BreeKorir <violetlamai521@gmail.com>
2025-03-13 20:28:29 +0300

    progress

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 const openMenuButton=document.querySelector("#inc");
 openMenuButton.addEventListener("click", ()=>{
     console.log("Opening Menu!!!")
-    document.querySelector("#mobile-menu").style.display="flex"})
+   document.querySelector("#mobile-menu").style.display="flex"})
 
 
     
 const closeMenuButton=document.querySelector("#ind");
 closeMenuButton.addEventListener("click", ()=>{
-    console.log("Closing Menu!!!")
-    document.querySelector("#mobile-menu").style.display="none"})
+   console.log("Closing Menu!!!")
+   document.querySelector("#mobile-menu").style.display="none"})
